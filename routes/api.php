63a4e095d991c34271c636fee4631885090fbd03
psone_phyo <psone9921@gmail.com>
2024-12-07 17:29:26 +0630

    fixed

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,11 @@ use App\Http\Controllers\api\PostController;
 use App\Http\Controllers\api\CategoryController;
 use App\Http\Controllers\api\AuthenticationController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-// Route::post('user/login', LoginController::class, 'login');
-// Route::get('user/', LoginController::class, 'index');
-
 //Auth
 Route::post('/user/login', [AuthenticationController::class,'login']);
 Route::post('/user/register', [AuthenticationController::class,'register']);
 
-// Route::group(['middleware' => 'auth:sanctum'], function(){
+Route::group(['middleware' => 'auth:sanctum'], function(){
 
     //posts
     Route::get('/posts', [PostController::class, 'index']);
@@ -32,4 +26,4 @@ Route::post('/user/register', [AuthenticationController::class,'register']);
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/categories/search', [CategoryController::class, 'search']);
 
-// });
+});
